6d03ecc501900ec318bd5e148b4f78320c0854c0
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2020-06-03 20:07:08 +0530

    controller class validation

--- src/main/java/com/zeeshan/form/controller/FormController.java
+++ src/main/java/com/zeeshan/form/controller/FormController.java
@@ -3,9 +3,12 @@ package com.zeeshan.form.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,8 +40,11 @@ public class FormController {
 	}
 
 	@PostMapping("/register")
-	public String submitForm(@ModelAttribute("user") User user) {
+	public String submitForm(@Valid @ModelAttribute("user") User user, BindingResult result) {
 
+		if (result.hasErrors()) {
+			return "register_form";
+		}
 		userRepository.save(user);
 		return "register_success";
 	}
