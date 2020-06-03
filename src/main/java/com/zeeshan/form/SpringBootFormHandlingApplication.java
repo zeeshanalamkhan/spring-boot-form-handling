d5d6fbca2ebdbeccb886340f1add576a1c666c92
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2020-06-03 12:45:54 +0530

    Spring boot bootstrap class containing main method

---
+++ src/main/java/com/zeeshan/form/SpringBootFormHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.zeeshan.form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFormHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFormHandlingApplication.class, args);
+	}
+
+}
